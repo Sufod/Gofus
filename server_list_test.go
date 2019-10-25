ece7054ccca11256fa218294a22129538210e258
Alexandre Gerez <nextriesh@gmail.com>
2019-10-25 18:48:35 +0200

    Handling game server list

---
+++ server_list_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetServersFromPacket(t *testing.T) {
+	var serverList ServerList
+	var err string
+	regularpacket := "AH601;1;110;0|605;1;110;0|609;1;110;0|604;1;110;0|608;1;110;0|603;1;110;0|607;1;110;0|611;1;110;0|602;1;110;0|606;1;110;0|610;1;110;0"
+
+	serverList.servers, err = getServersFromPacket(regularpacket) //expected = [{601} {605} {609} {604} {608} {603} {607} {611} {602} {606} {610}]
+	if len(serverList.servers) != 11 {
+		fmt.Println(err)
+		t.Error()
+	}
+
+	emptypacket := "AH"
+	serverList.servers, err = getServersFromPacket(emptypacket) //expected = []
+	if len(serverList.servers) != 0 {
+		fmt.Println(err)
+		t.Error()
+	}
+}
